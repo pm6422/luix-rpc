6eab60b54dbeb8b7846cf8543ae986362bd8a71c
Louis <pm6422@126.com>
2020-01-20 22:55:49 +0800

    refactor

--- spring-boot-starter-infinity-rpc/src/main/java/org/infinity/springboot/infinityrpc/RpcConfigurationApplicationContextInitializer.java
+++ spring-boot-starter-infinity-rpc/src/main/java/org/infinity/springboot/infinityrpc/RpcConfigurationApplicationContextInitializer.java
@@ -1,41 +1,41 @@
-//package org.infinity.springboot.infinityrpc;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.infinity.rpc.core.SpringBeanDefinitionRegistryPostProcessor;
-//import org.infinity.rpc.core.SpringBeanPostProcessor;
-//import org.springframework.context.ApplicationContextInitializer;
-//import org.springframework.context.ConfigurableApplicationContext;
-//import org.springframework.core.env.Environment;
-//import org.springframework.util.Assert;
-//import org.springframework.util.ClassUtils;
-//
-///**
-// * SpringBoot的SPI扩展在META-INF/spring.factories中配置
-// */
-//@Slf4j
-//public class RpcConfigurationApplicationContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-//    @Override
-//    public void initialize(ConfigurableApplicationContext applicationContext) {
-//        Environment env = applicationContext.getEnvironment();
+package org.infinity.springboot.infinityrpc;
+
+import lombok.extern.slf4j.Slf4j;
+import org.infinity.rpc.core.client.ConsumerBeanPostProcessor;
+import org.infinity.rpc.core.server.ProviderBeanDefinitionRegistryPostProcessor;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.util.ClassUtils;
+
+/**
+ * SpringBoot的SPI扩展在META-INF/spring.factories中配置
+ */
+@Slf4j
+@Deprecated
+public class RpcConfigurationApplicationContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        Environment env = applicationContext.getEnvironment();
 //        String scanPackages = env.getProperty(InfinityRpcProperties.SCAN_PACKAGES);
 //        Assert.hasText(scanPackages, "Consumer and provider scan base packages must not be empty!");
-//
+
 //        log.debug("RPC scan base package: [{}]", scanPackages);
-//        SpringBeanPostProcessor springBeanPostProcessor = new SpringBeanPostProcessor(applicationContext, scanPackages);
-//        // Bean post processor will be called before bean factory post processor
+//        ConsumerBeanPostProcessor springBeanPostProcessor = new ConsumerBeanPostProcessor(scanPackages);
+        // Bean post processor will be called before bean factory post processor
 //        applicationContext.getBeanFactory().addBeanPostProcessor(springBeanPostProcessor);
 //        applicationContext.addBeanFactoryPostProcessor(springBeanPostProcessor);
-//        String springBeanPostProcessorBeanName = ClassUtils.getShortNameAsProperty(SpringBeanPostProcessor.class);
-//        // 注意：这里只有注册bean，但由于时机太早，没法注册beanDefinition，所以applicationContext.getBeanDefinitionNames()里获取不到
-//        // 但applicationContext.getBean(ConsumerAnnotationBean.class)可以获取到bean
-//        // Register custom bean post processor
+        String springBeanPostProcessorBeanName = ClassUtils.getShortNameAsProperty(ConsumerBeanPostProcessor.class);
+        // 注意：这里只有注册bean，但由于时机太早，没法注册beanDefinition，所以applicationContext.getBeanDefinitionNames()里获取不到
+        // 但applicationContext.getBean(ConsumerAnnotationBean.class)可以获取到bean
+        // Register custom bean post processor
 //        applicationContext.getBeanFactory().registerSingleton(springBeanPostProcessorBeanName, springBeanPostProcessor);
-//
-//        SpringBeanDefinitionRegistryPostProcessor springBeanDefinitionRegistryPostProcessor = new SpringBeanDefinitionRegistryPostProcessor();
-//        // Register custom bean definition post processor
-//        String springBeanDefinitionRegistryPostProcessorBeanName = ClassUtils.getShortNameAsProperty(SpringBeanDefinitionRegistryPostProcessor.class);
+
+//        ProviderBeanDefinitionRegistryPostProcessor springBeanDefinitionRegistryPostProcessor = new ProviderBeanDefinitionRegistryPostProcessor(scanPackages);
+        // Register custom bean definition post processor
+        String springBeanDefinitionRegistryPostProcessorBeanName = ClassUtils.getShortNameAsProperty(ProviderBeanDefinitionRegistryPostProcessor.class);
 //        applicationContext.getBeanFactory().registerSingleton(springBeanDefinitionRegistryPostProcessorBeanName, springBeanDefinitionRegistryPostProcessor);
-//
-//        log.debug("Initialized consumer annotation bean");
-//    }
-//}
+
+        log.debug("Initialized consumer annotation bean");
+    }
+}
