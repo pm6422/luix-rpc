ee8ee0589359fbc089c8ca2ddac47e60000b606a
louis <pm6422@126.com>
2020-03-27 19:04:10 +0800

    upgrade springboot version

--- app-server/src/main/java/org/infinity/rpc/appserver/config/MongoConfiguration.java
+++ app-server/src/main/java/org/infinity/rpc/appserver/config/MongoConfiguration.java
@@ -2,36 +2,36 @@ package org.infinity.rpc.appserver.config;
 
 import com.github.mongobee.Mongobee;
 import com.mongodb.MongoClient;
+import lombok.extern.slf4j.Slf4j;
 import org.infinity.rpc.appserver.setup.DatabaseInitialSetup;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
-import org.springframework.data.mongodb.core.convert.*;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.index.IndexOperations;
+import org.springframework.data.mongodb.core.index.MongoPersistentEntityIndexResolver;
+import org.springframework.data.mongodb.core.mapping.BasicMongoPersistentEntity;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Configuration
+//@Import(value = MongoAutoConfiguration.class)
+@Slf4j
 public class MongoConfiguration {
 
-    private final Logger               LOGGER = LoggerFactory.getLogger(MongoConfiguration.class);
     @Autowired
-    private       MongoMappingContext  mongoMappingContext;
+    private MongoMappingContext mongoMappingContext;
     @Autowired
-    private       SimpleMongoDbFactory mongoDbFactory;
-
-    @Bean
-    public LocalValidatorFactoryBean validator() {
-        return new LocalValidatorFactoryBean();
-    }
+    private MongoDbFactory      mongoDbFactory;
 
     @Bean
     public ValidatingMongoEventListener validatingMongoEventListener() {
@@ -39,36 +39,49 @@ public class MongoConfiguration {
     }
 
     @Bean
-    public MongoCustomConversions customConversions() {
-        List<Converter<?, ?>> converters = new ArrayList<>();
-        return new MongoCustomConversions(converters);
+    public LocalValidatorFactoryBean validator() {
+        return new LocalValidatorFactoryBean();
     }
+
 
     @Bean
     public MappingMongoConverter mappingMongoConverter() {
         DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDbFactory);
         MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
-        converter.setCustomConversions(customConversions());
         // remove _class field
         converter.setTypeMapper(new DefaultMongoTypeMapper(null));
         return converter;
     }
 
     @Bean
-    public MongoTemplate mongoTemplate() throws Exception {
+    public MongoTemplate mongoTemplate() {
         return new MongoTemplate(mongoDbFactory, mappingMongoConverter());
     }
 
     @Bean
     public Mongobee mongobee(MongoClient mongoClient, MongoTemplate mongoTemplate) {
-        LOGGER.debug("Configuring Mongobee");
+        log.debug("Configuring Mongobee");
         Mongobee mongobee = new Mongobee(mongoClient);
         // For embedded mongo
         mongobee.setDbName(mongoClient.listDatabaseNames().first());
         mongobee.setMongoTemplate(mongoTemplate);
         mongobee.setChangeLogsScanPackage(DatabaseInitialSetup.class.getPackage().getName());
         mongobee.setEnabled(true);
-        LOGGER.debug("Configured Mongobee");
+        log.debug("Configured Mongobee");
         return mongobee;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void initIndicesAfterStartup() {
+        if (mongoMappingContext instanceof MongoMappingContext) {
+            for (BasicMongoPersistentEntity<?> persistentEntity : mongoMappingContext.getPersistentEntities()) {
+                Class clazz = persistentEntity.getType();
+                if (clazz.isAnnotationPresent(Document.class)) {
+                    MongoPersistentEntityIndexResolver resolver = new MongoPersistentEntityIndexResolver(mongoMappingContext);
+                    IndexOperations indexOps = mongoTemplate().indexOps(clazz);
+                    resolver.resolveIndexFor(clazz).forEach(indexOps::ensureIndex);
+                }
+            }
+        }
     }
 }
