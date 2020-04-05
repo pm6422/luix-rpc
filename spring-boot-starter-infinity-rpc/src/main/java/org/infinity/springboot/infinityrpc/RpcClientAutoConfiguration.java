8fe3b094c81d2dc004d2b6a3fc1d8cd0330409ef
louis <pm6422@126.com>
2020-04-05 10:09:43 +0800

    refactor

--- spring-boot-starter-infinity-rpc/src/main/java/org/infinity/springboot/infinityrpc/RpcClientAutoConfiguration.java
+++ spring-boot-starter-infinity-rpc/src/main/java/org/infinity/springboot/infinityrpc/RpcClientAutoConfiguration.java
@@ -2,37 +2,20 @@ package org.infinity.springboot.infinityrpc;
 
 import lombok.extern.slf4j.Slf4j;
 import org.infinity.rpc.core.client.proxy.RpcConsumerProxy;
-import org.infinity.rpc.core.server.RpcServer;
 import org.infinity.rpc.registry.ZkRpcServerRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Slf4j
-@EnableConfigurationProperties({InfinityRpcProperties.class})
 @Configuration
 public class RpcClientAutoConfiguration {
 
     @Autowired
-    private InfinityRpcProperties infinityRpcProperties;
-
-    @Autowired
-    private ApplicationContext applicationContext;
-
-    @Bean
-    public ZkRpcServerRegistry rpcServiceRegistry() {
-        return new ZkRpcServerRegistry(infinityRpcProperties.getRegistry().getAddress());
-    }
+    private ZkRpcServerRegistry rpcServiceRegistry;
 
     @Bean
     public RpcConsumerProxy rpcConsumerProxy(ZkRpcServerRegistry rpcServiceRegistry) {
         return new RpcConsumerProxy(rpcServiceRegistry);
-    }
-
-    @Bean
-    public RpcServer rpcServer() {
-        return new RpcServer(infinityRpcProperties.getProtocol().getPort(), rpcServiceRegistry());
     }
 }
---
+++ spring-boot-starter-infinity-rpc/src/main/java/org/infinity/springboot/infinityrpc/RpcServerAutoConfiguration.java
@@ -0,0 +1,33 @@
+package org.infinity.springboot.infinityrpc;
+
+import lombok.extern.slf4j.Slf4j;
+import org.infinity.rpc.core.client.proxy.RpcConsumerProxy;
+import org.infinity.rpc.core.server.RpcServer;
+import org.infinity.rpc.registry.ZkRpcServerRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@EnableConfigurationProperties({InfinityRpcProperties.class})
+@Configuration
+public class RpcServerAutoConfiguration {
+
+    @Autowired
+    private InfinityRpcProperties infinityRpcProperties;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Bean
+    public ZkRpcServerRegistry rpcServiceRegistry() {
+        return new ZkRpcServerRegistry(infinityRpcProperties.getRegistry().getAddress());
+    }
+
+    @Bean
+    public RpcServer rpcServer() {
+        return new RpcServer(infinityRpcProperties.getProtocol().getPort(), rpcServiceRegistry());
+    }
+}
