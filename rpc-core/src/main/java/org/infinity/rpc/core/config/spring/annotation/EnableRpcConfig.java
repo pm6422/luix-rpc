8214bd978d09651ed4de7f915e759f096c54fa31
louis <pm6422@126.com>
2020-04-13 09:32:25 +0800

    remove spring boot module

--- rpc-core/src/main/java/org/infinity/rpc/core/config/spring/annotation/EnableRpcConfig.java
+++
@@ -1,8 +0,0 @@
-package org.infinity.rpc.core.config.spring.annotation;
-
-import org.infinity.rpc.core.config.spring.registrar.RpcConsumerProviderScanRegistrar;
-import org.springframework.context.annotation.Import;
-
-//@Import(RpcConsumerProviderScanRegistrar.class)
-//public @interface EnableRpcConfig {
-//}
