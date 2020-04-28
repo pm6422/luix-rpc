893187d04748a0dec57234400901242f2a4afb19
louis <pm6422@126.com>
2020-04-28 10:38:55 +0800

    implement registry

--- rpc-core/src/main/java/org/infinity/rpc/core/registry/Url.java
+++ rpc-core/src/main/java/org/infinity/rpc/core/registry/Url.java
@@ -17,7 +17,7 @@ import java.util.concurrent.ConcurrentHashMap;
 @EqualsAndHashCode
 public class Url implements Serializable {
     private static final long    serialVersionUID   = 2970867582138131181L;
-    // {protocol}://{host}:{port}/{service}?{parameterPairs}
+    // URL Pattern: {protocol}://{host}:{port}/{path}?{parameters}
     private static final String  URL_PATTERN        = "{0}://{1}:{2}/{3}?{4}";
     public static final  String  PROTOCOL_SEPARATOR = "://";
     public static final  String  PATH_SEPARATOR     = "/";
