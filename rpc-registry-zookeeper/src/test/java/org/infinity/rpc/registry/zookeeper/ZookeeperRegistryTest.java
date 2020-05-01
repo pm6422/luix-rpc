540b2fa05d3789ab8dd786b1cf0af8600d2a8b7c
louis <pm6422@126.com>
2020-05-01 19:45:07 +0800

    fixed the test case

--- rpc-registry-zookeeper/src/test/java/org/infinity/rpc/registry/zookeeper/ZookeeperRegistryTest.java
+++ rpc-registry-zookeeper/src/test/java/org/infinity/rpc/registry/zookeeper/ZookeeperRegistryTest.java
@@ -42,10 +42,10 @@ public class ZookeeperRegistryTest {
         registryUrl.addParameter(Url.PARAM_SESSION_TIMEOUT, new InfinityProperties().getRegistry().getSessionTimeout().toString());
         registryUrl.addParameter(Url.PARAM_RETRY_INTERVAL, new InfinityProperties().getRegistry().getRetryInterval().toString());
 
-        clientUrl = Url.of("rpc", "127.0.0.1", 0, provider);
+        clientUrl = Url.of("infinity", "127.0.0.1", 0, provider);
         clientUrl.addParameter("group", Url.PARAM_GROUP_VALUE);
 
-        providerUrl = Url.of("infinity", "127.0.0.1", 8001, provider);
+        providerUrl = Url.of("zookeeper", "127.0.0.1", 8001, provider);
         providerUrl.addParameter("group", Url.PARAM_GROUP_VALUE);
 
         zookeeper = new EmbeddedZookeeper();
