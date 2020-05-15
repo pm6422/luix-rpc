1df96a8e3df87663ae9e815a24d46ea5b5d98704
louis <pm6422@126.com>
2020-05-15 09:50:17 +0800

    refactor

--- rpc-core/src/main/java/org/infinity/rpc/core/registry/FailbackAbstractRegistry.java
+++ rpc-core/src/main/java/org/infinity/rpc/core/registry/FailbackAbstractRegistry.java
@@ -260,7 +260,7 @@ public abstract class FailbackAbstractRegistry extends AbstractRegistry {
      * @return true: all the urls contain PARAM_CHECK_HEALTH, false: any url does not contain PARAM_CHECK_HEALTH
      */
     private boolean checkAllHealth(Url... urls) {
-        return Arrays.asList(urls).stream().allMatch(url -> Boolean.parseBoolean(url.getParameter(Url.PARAM_CHECK_HEALTH)));
+        return !Arrays.asList(urls).stream().anyMatch(url -> !Boolean.parseBoolean(url.getParameter(Url.PARAM_CHECK_HEALTH)));
     }
 
     /**
