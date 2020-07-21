9c95dd29f6c4ee6fb0fe984b03fff4ecf8cb1674
louis <pm6422@126.com>
2020-07-21 10:47:41 +0800

    add exchange module

---
+++ rpc-utilities/src/test/java/org/infinity/rpc/utilities/id/SequenceTest2.java
@@ -0,0 +1,34 @@
+package org.infinity.rpc.utilities.id;
+
+public class SequenceTest2 {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("场景一：毫秒内固定起始值开始");
+        testSequence();
+        System.out.println("场景二：毫秒内随机起始值开始");
+        testRandomSequence();
+    }
+
+    /**
+     * 场景一：毫秒内固定起始值开始
+     */
+    private static void testSequence() throws Exception {
+        Sequence sequence = new Sequence(1L, false, false);
+        for (int i = 0; i < 100; i++) {
+            Thread.sleep(1);
+            System.out.println(sequence.nextId());
+        }
+    }
+
+    /**
+     * 场景二：毫秒内随机起始值开始
+     */
+    private static void testRandomSequence() throws Exception {
+        Sequence sequence = new Sequence(1L, false, true);
+        for (int i = 0; i < 100; i++) {
+            Thread.sleep(1);
+            System.out.println(sequence.nextId());
+        }
+    }
+
+}
