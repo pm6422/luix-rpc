337958b565f73fd869e41f38983b7f274bc69979
louis <pm6422@126.com>
2020-07-30 16:44:48 +0800

    refactor package

--- rpc-core/src/main/java/org/infinity/rpc/core/registry/Registry.java
+++ rpc-core/src/main/java/org/infinity/rpc/core/registry/Registry.java
@@ -3,16 +3,11 @@ package org.infinity.rpc.core.registry;
 import org.infinity.rpc.core.url.Url;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Registry interface
  */
 public interface Registry extends Registrable, Subscribable {
-
-    AtomicReference<List<Url>>      REGISTRY_URL_CACHE = new AtomicReference<>();
-    AtomicReference<List<Registry>> REGISTRY_CACHE     = new AtomicReference<>();
-
     /**
      * Get registry subclass name
      *
