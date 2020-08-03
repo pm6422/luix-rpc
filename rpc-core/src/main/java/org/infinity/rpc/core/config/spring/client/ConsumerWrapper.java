49a3b5baba85476d661dca88971acc9ef0f47587
louis <pm6422@126.com>
2020-08-03 11:09:22 +0800

    refactor

--- rpc-core/src/main/java/org/infinity/rpc/core/config/spring/client/ConsumerWrapper.java
+++ rpc-core/src/main/java/org/infinity/rpc/core/config/spring/client/ConsumerWrapper.java
@@ -1,6 +1,5 @@
 package org.infinity.rpc.core.config.spring.client;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.infinity.rpc.core.client.proxy.RpcConsumerProxy;
