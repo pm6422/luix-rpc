d56413591a4f9a1571a331611d6da70a3e5fb438
louis <pm6422@126.com>
2020-10-22 00:01:19 +0800

    refactor codes

--- rpc-demo/app-client/src/main/java/org/infinity/rpc/appclient/AppClientLauncher.java
+++ rpc-demo/app-client/src/main/java/org/infinity/rpc/appclient/AppClientLauncher.java
@@ -1,61 +1,48 @@
 package org.infinity.rpc.appclient;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ArrayUtils;
 import org.infinity.rpc.appclient.config.ApplicationConstants;
 import org.infinity.rpc.core.config.spring.annotation.EnableRpc;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
 import org.springframework.util.Assert;
 
 import javax.annotation.PostConstruct;
-import java.io.IOException;
 import java.util.Arrays;
 
 @EnableRpc
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class, MongoDataAutoConfiguration.class})
-public class AppClientLauncher implements ApplicationContextAware {
+@Slf4j
+public class AppClientLauncher {
+    private final Environment env;
 
-    private static final Logger                         LOGGER = LoggerFactory.getLogger(AppClientLauncher.class);
-    @Autowired
-    private              Environment                    env;
-    private static       ConfigurableApplicationContext applicationContext;
+    public AppClientLauncher(Environment env) {
+        this.env = env;
+    }
 
     /**
      * Entrance method which used to run the application. Spring profiles can be configured with a program arguments
      * --spring.profiles.active=your-active-profile
      *
-     * @param args
-     * @throws IOException
+     * @param args program arguments
      */
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(AppClientLauncher.class);
-        app.run(args);
+        SpringApplication.run(AppClientLauncher.class, args);
     }
 
     @PostConstruct
     private void validateProfiles() {
         Assert.notEmpty(env.getActiveProfiles(), "No Spring profile configured.");
         Assert.isTrue(env.getActiveProfiles().length == 1, "Multiple profiles are not allowed!");
-        Arrays.asList(env.getActiveProfiles()).stream()
+        Arrays.stream(env.getActiveProfiles())
                 .filter(activeProfile -> !ArrayUtils.contains(ApplicationConstants.AVAILABLE_PROFILES, activeProfile))
                 .findFirst().ifPresent((activeProfile) -> {
-            LOGGER.error("Misconfigured application with an illegal profile '{}'!", activeProfile);
+            log.error("Mis-configured application with an illegal profile '{}'!", activeProfile);
             System.exit(0);
         });
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = (ConfigurableApplicationContext) applicationContext;
     }
 }
--- rpc-web-center/src/main/java/org/infinity/rpc/webcenter/WebCenterLauncher.java
+++ rpc-web-center/src/main/java/org/infinity/rpc/webcenter/WebCenterLauncher.java
@@ -3,9 +3,6 @@ package org.infinity.rpc.webcenter;
 import org.infinity.rpc.core.config.spring.annotation.EnableRpc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-
-import java.io.IOException;
 
 @SpringBootApplication
 @EnableRpc
@@ -14,11 +11,9 @@ public class WebCenterLauncher {
      * Entrance method which used to run the application. Spring profiles can be configured with a program arguments
      * --spring.profiles.active=your-active-profile
      *
-     * @param args
-     * @throws IOException
+     * @param args program arguments
      */
-    public static void main(String[] args) throws Exception {
-        SpringApplication app = new SpringApplication(WebCenterLauncher.class);
-        app.run(args);
+    public static void main(String[] args) {
+        SpringApplication.run(WebCenterLauncher.class, args);
     }
 }
