77dc3a132f86dddcb9cb1310d89a9196ec574b96
louis <pm6422@126.com>
2020-12-17 14:34:00 +0800

    refactor error handling

--- rpc-demo/app-common/src/main/java/org/infinity/app/common/dto/AuthorityDTO.java
+++ rpc-demo/app-common/src/main/java/org/infinity/app/common/dto/AuthorityDTO.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 /**
@@ -16,6 +19,9 @@ public class AuthorityDTO implements Serializable {
 
     private static final long serialVersionUID = 6131756179263179005L;
 
+    @NotNull
+    @Size(min = 3, max = 16)
+    @Pattern(regexp = "^[A-Z_]+$", message = "{EP5902}")
     private String name;
 
     private Boolean systemLevel;
