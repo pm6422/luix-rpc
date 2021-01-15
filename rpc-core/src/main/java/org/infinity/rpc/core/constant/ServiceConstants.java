870664d9b94b480356b412da61c772d68f25f57f
louis <pm6422@126.com>
2021-01-15 17:35:59 +0800

    refactor

--- rpc-core/src/main/java/org/infinity/rpc/core/constant/ServiceConstants.java
+++ rpc-core/src/main/java/org/infinity/rpc/core/constant/ServiceConstants.java
@@ -1,14 +1,14 @@
 package org.infinity.rpc.core.constant;
 
 public interface ServiceConstants {
-    String INTERFACE_CLASS           = "interfaceClass";
-    String INTERFACE_NAME            = "interfaceName";
-    String GENERIC                   = "generic";
-    String PROTOCOL                  = "protocol";
-    String GROUP                     = "group";
-    String GROUP_DEFAULT_VALUE       = "default";
-    String VERSION                   = "version";
-    String VERSION_DEFAULT_VALUE     = "1.0.0";
-    int    MAX_RETRIES_DEFAULT_VALUE = 0;
-    int     REQUEST_TIMEOUT_DEFAULT_VALUE       = 500;
+    String INTERFACE_CLASS               = "interfaceClass";
+    String INTERFACE_NAME                = "interfaceName";
+    String GENERIC                       = "generic";
+    String PROTOCOL                      = "protocol";
+    String GROUP                         = "group";
+    String GROUP_DEFAULT_VALUE           = "default";
+    String VERSION                       = "version";
+    String VERSION_DEFAULT_VALUE         = "1.0.0";
+    int    MAX_RETRIES_DEFAULT_VALUE     = 0;
+    int    REQUEST_TIMEOUT_DEFAULT_VALUE = 500;
 }
