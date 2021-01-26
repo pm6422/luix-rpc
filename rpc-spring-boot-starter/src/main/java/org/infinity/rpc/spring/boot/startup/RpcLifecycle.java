501e007da4465fce1a30b9d88a36721471f525dc
louis <pm6422@126.com>
2021-01-26 13:44:28 +0800

    refactor

--- rpc-spring-boot-starter/src/main/java/org/infinity/rpc/spring/boot/startup/RpcLifecycle.java
+++ rpc-spring-boot-starter/src/main/java/org/infinity/rpc/spring/boot/startup/RpcLifecycle.java
@@ -5,6 +5,7 @@ import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.time.DateFormatUtils;
 import org.infinity.rpc.core.config.ApplicationConfig;
 import org.infinity.rpc.core.config.ApplicationExtConfig;
+import org.infinity.rpc.core.config.RegistryConfig;
 import org.infinity.rpc.core.exchange.client.stub.ConsumerStub;
 import org.infinity.rpc.core.exchange.client.stub.ConsumerStubHolder;
 import org.infinity.rpc.core.exchange.server.stub.ProviderStub;
@@ -98,23 +99,33 @@ public class RpcLifecycle {
     private void publish(InfinityProperties infinityProperties) {
         infinityProperties.getRegistryConfigs().forEach(registryConfig -> {
             Registry registry = registryConfig.getRegistryImpl();
-//            ApplicationExtConfig application = getApplicationExtConfig(infinityProperties.getApplication());
-//            registry.registerApplication(application);
-//            log.debug("Registered RPC server application [{}] to registry [{}]",
-//                    infinityProperties.getApplication().getName(), registryConfig.getName());
-
-            Map<String, ProviderStub<?>> providerStubs = ProviderStubHolder.getInstance().getStubs();
-            if (MapUtils.isEmpty(providerStubs)) {
-                log.info("No RPC service providers found to register to registry [{}]!", registryConfig.getName());
-                return;
-            }
-            providerStubs.forEach((name, providerStub) -> {
-                // Register providers
-                providerStub.register(infinityProperties.getApplication(), infinityProperties.getAvailableProtocol(),
-                        registryConfig, infinityProperties.getProvider(), registry);
-            });
-            SwitcherService.getInstance().setValue(SwitcherService.REGISTRY_HEARTBEAT_SWITCHER, true);
+            // Publish application first
+            publishApplication(infinityProperties, registryConfig, registry);
+            // Publish providers next
+            publishProviders(infinityProperties, registryConfig, registry);
         });
+    }
+
+    private void publishApplication(InfinityProperties infinityProperties, RegistryConfig registryConfig, Registry registry) {
+        ApplicationExtConfig application = getApplicationExtConfig(infinityProperties.getApplication());
+        registry.registerApplication(application);
+        log.debug("Registered RPC server application [{}] to registry [{}]",
+                infinityProperties.getApplication().getName(), registryConfig.getName());
+    }
+
+    private void publishProviders(InfinityProperties infinityProperties, RegistryConfig registryConfig, Registry registry) {
+        Map<String, ProviderStub<?>> providerStubs = ProviderStubHolder.getInstance().getStubs();
+        if (MapUtils.isEmpty(providerStubs)) {
+            log.info("No RPC service providers found to register to registry [{}]!", registryConfig.getName());
+            return;
+        }
+        providerStubs.forEach((name, providerStub) -> {
+            // Register providers
+            providerStub.register(infinityProperties.getApplication(), infinityProperties.getAvailableProtocol(),
+                    registryConfig, infinityProperties.getProvider(), registry);
+        });
+
+        SwitcherService.getInstance().setValue(SwitcherService.REGISTRY_HEARTBEAT_SWITCHER, true);
     }
 
     private ApplicationExtConfig getApplicationExtConfig(ApplicationConfig applicationConfig) {
