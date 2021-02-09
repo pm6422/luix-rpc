9e123a43a49e2057ecdd9181a9cd868f0ab873f1
louis <pm6422@126.com>
2021-02-09 22:43:05 +0800

    refactor

--- rpc-demo/rpc-demo-client/src/main/java/org/infinity/rpc/democlient/controller/SystemController.java
+++ rpc-demo/rpc-demo-client/src/main/java/org/infinity/rpc/democlient/controller/SystemController.java
@@ -1,20 +1,27 @@
 package org.infinity.rpc.democlient.controller;
 
 import io.swagger.annotations.Api;
-import org.infinity.rpc.democlient.config.ApplicationProperties;
+import io.swagger.annotations.ApiParam;
 import org.infinity.rpc.utilities.network.NetworkUtils;
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Api(tags = "系统")
 public class SystemController {
 
-    private final ApplicationProperties applicationProperties;
+    private final ApplicationContext applicationContext;
 
-    public SystemController(ApplicationProperties applicationProperties) {
-        this.applicationProperties = applicationProperties;
+    public SystemController(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @GetMapping("/api/system/bean")
+    public ResponseEntity<Object> getBean(@ApiParam(value = "bean名称", required = true) @RequestParam(value = "name") String name) {
+        return ResponseEntity.ok(applicationContext.getBean(name));
     }
 
     @GetMapping("/api/system/internet-ip")
