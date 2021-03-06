6e6cdd89661e7da8b6b62796f43ec9c3da136aa4
louis <pm6422@126.com>
2021-03-06 23:37:14 +0800

    upgrade to springboot 2.3

--- rpc-demo/rpc-demo-server/src/main/java/org/infinity/rpc/demoserver/db/initializer/DatabaseInitializer.java
+++ rpc-demo/rpc-demo-server/src/main/java/org/infinity/rpc/demoserver/db/initializer/DatabaseInitializer.java
@@ -1,28 +1,28 @@
-package org.infinity.rpc.demoserver.setup;
+package org.infinity.rpc.demoserver.db.initializer;
 
-import com.github.mongobee.changeset.ChangeLog;
-import com.github.mongobee.changeset.ChangeSet;
+import com.github.cloudyrock.mongock.ChangeLog;
+import com.github.cloudyrock.mongock.ChangeSet;
+import com.github.cloudyrock.mongock.driver.mongodb.springdata.v3.decorator.impl.MongockTemplate;
 import org.infinity.rpc.democommon.domain.AdminMenu;
 import org.infinity.rpc.democommon.domain.App;
 import org.infinity.rpc.democommon.domain.Authority;
-import org.springframework.data.mongodb.core.MongoTemplate;
 
 /**
  * Creates the initial database
  */
-@ChangeLog(order = "001")
-public class DatabaseInitialSetup {
+@ChangeLog(order = "01")
+public class DatabaseInitializer {
 
     private static final String APP_NAME = "rpc-demo-server";
 
     @ChangeSet(order = "01", author = "Louis", id = "addApps", runAlways = true)
-    public void addApps(MongoTemplate mongoTemplate) {
+    public void addApps(MongockTemplate mongoTemplate) {
         App app = new App(APP_NAME, true);
         mongoTemplate.save(app);
     }
 
     @ChangeSet(order = "02", author = "Louis", id = "addAuthorities", runAlways = true)
-    public void addAuthorities(MongoTemplate mongoTemplate) {
+    public void addAuthorities(MongockTemplate mongoTemplate) {
         mongoTemplate.save(new Authority(Authority.USER, true, true));
         mongoTemplate.save(new Authority(Authority.ADMIN, true, true));
         mongoTemplate.save(new Authority(Authority.DEVELOPER, true, true));
@@ -30,7 +30,7 @@ public class DatabaseInitialSetup {
     }
 
     @ChangeSet(order = "04", author = "Louis", id = "addAuthorityAdminMenu", runAlways = true)
-    public void addAuthorityAdminMenu(MongoTemplate mongoTemplate) {
+    public void addAuthorityAdminMenu(MongockTemplate mongoTemplate) {
 
         AdminMenu userAuthority = new AdminMenu("user-authority", "用户权限", 1, "user-authority", 100, null);
         mongoTemplate.save(userAuthority);
@@ -45,5 +45,4 @@ public class DatabaseInitialSetup {
         AdminMenu appList = new AdminMenu("app-list", "应用", 2, "app.app-list", 201, app.getId());
         mongoTemplate.save(appList);
     }
-
 }
