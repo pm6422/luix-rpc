be375f8f78ed193c55c2ebf0e959ba74ae958641
louis <pm6422@126.com>
2021-03-09 22:29:58 +0800

    refactor

--- rpc-core/src/main/java/org/infinity/rpc/core/client/cluster/impl/DefaultInvokerCluster.java
+++ rpc-core/src/main/java/org/infinity/rpc/core/client/cluster/impl/DefaultInvokerCluster.java
@@ -67,7 +67,6 @@ public class DefaultInvokerCluster implements InvokerCluster {
         loadBalancer.destroy();
     }
 
-
     @Override
     public Responseable call(Requestable request) {
         if (active) {
