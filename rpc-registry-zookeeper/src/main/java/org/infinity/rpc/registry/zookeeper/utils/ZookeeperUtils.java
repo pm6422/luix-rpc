28a324f7df2a8fd5572a7583f23bde9efd72d5f0
louis <pm6422@126.com>
2021-03-31 22:33:04 +0800

    refactor zk registry

--- rpc-registry-zookeeper/src/main/java/org/infinity/rpc/registry/zookeeper/utils/ZookeeperUtils.java
+++ rpc-registry-zookeeper/src/main/java/org/infinity/rpc/registry/zookeeper/utils/ZookeeperUtils.java
@@ -5,14 +5,14 @@ import org.I0Itec.zkclient.ZkClient;
 import org.infinity.rpc.core.url.Url;
 import org.infinity.rpc.registry.zookeeper.ZookeeperStatusNode;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.apache.commons.io.IOUtils.DIR_SEPARATOR_UNIX;
 
 
 @Slf4j
-public class ZookeeperUtils {
+public abstract class ZookeeperUtils {
 
     public static final String REGISTRY_NAMESPACE = "/infinity";
     public static final String PROVIDER_DIR       = "/provider";
@@ -56,6 +56,16 @@ public class ZookeeperUtils {
     }
 
     /**
+     * Get all child directory or file names under the parent path
+     *
+     * @param path zookeeper directory path
+     * @return child node names
+     */
+    public static List<String> getChildrenNames(ZkClient zkClient, String path) {
+        return zkClient.exists(path) ? zkClient.getChildren(path) : Collections.emptyList();
+    }
+
+    /**
      * Get the command full path
      *
      * @param url url
@@ -63,20 +73,6 @@ public class ZookeeperUtils {
      */
     public static String getCommandPath(Url url) {
         return getFormPath(url.getForm()) + REGISTRY_COMMAND;
-    }
-
-    /**
-     * Get all child directory or file names under the parent path
-     *
-     * @param path zookeeper directory path
-     * @return child node names
-     */
-    public static List<String> getChildrenNames(ZkClient zkClient, String path) {
-        List<String> children = new ArrayList<>();
-        if (zkClient.exists(path)) {
-            children = zkClient.getChildren(path);
-        }
-        return children;
     }
 
     /**
@@ -88,50 +84,4 @@ public class ZookeeperUtils {
     public static String getFormPath(String form) {
         return PROVIDER_PATH + DIR_SEPARATOR_UNIX + form;
     }
-
-    /**
-     * Get providers by form
-     *
-     * @param zkClient zk client
-     * @param form     form
-     * @return provider with status form
-     */
-//    public static Map<String, Map<String, List<AddressInfo>>> getAllProviders(ZkClient zkClient, String form) {
-//        List<String> providerPaths = getChildrenNames(zkClient, getFormPath(form));
-//        Map<String, Map<String, List<AddressInfo>>> results = new HashMap<>(providerPaths.size());
-//        for (String providerPath : providerPaths) {
-//            Map<String, List<AddressInfo>> providersPerStatus = new HashMap<>();
-//            List<AddressInfo> activeProviders = getProviderAddresses(zkClient, form, providerPath, ZookeeperStatusNode.ACTIVE);
-//            providersPerStatus.put(ZookeeperStatusNode.ACTIVE.getValue(), activeProviders);
-//            List<AddressInfo> inactiveProviders = getProviderAddresses(zkClient, form, providerPath, ZookeeperStatusNode.INACTIVE);
-//            providersPerStatus.put(ZookeeperStatusNode.INACTIVE.getValue(), inactiveProviders);
-//            List<AddressInfo> clientProviders = getProviderAddresses(zkClient, form, providerPath, ZookeeperStatusNode.CLIENT);
-//            providersPerStatus.put(ZookeeperStatusNode.CLIENT.getValue(), clientProviders);
-//
-//            results.put(providerPath, providersPerStatus);
-//        }
-//        return results;
-//    }
-
-    /**
-     * @param zkClient     zk client
-     * @param form         provider form
-     * @param providerPath provider path
-     * @param statusNode   status
-     * @return provider addresses
-     */
-//    public static List<AddressInfo> getProviderAddresses(ZkClient zkClient, String form, String providerPath,
-//                                                         ZookeeperStatusNode statusNode) {
-//        List<AddressInfo> result = new ArrayList<>();
-//        List<String> providerAddressNames = getChildrenNames(zkClient, getProviderStatusNodePath(form, providerPath, statusNode));
-//        for (String providerAddressName : providerAddressNames) {
-//            AddressInfo addressInfo = new AddressInfo();
-//            String providerAddressFilePath = getProviderAddressFilePath(form, providerPath, statusNode, providerAddressName);
-//            String info = zkClient.readData(providerAddressFilePath, true);
-//            addressInfo.setAddress(providerAddressName);
-//            addressInfo.setContents(info);
-//            result.add(addressInfo);
-//        }
-//        return result;
-//    }
 }
