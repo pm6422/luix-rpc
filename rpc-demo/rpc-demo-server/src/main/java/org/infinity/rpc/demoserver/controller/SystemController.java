bf721ab549206b8a65ab489393ba02b80d90ee78
刘尧 <liuyao@xforceplus.com>
2021-04-28 21:58:02 +0800

    refactor

--- rpc-demo/rpc-demo-server/src/main/java/org/infinity/rpc/demoserver/controller/SystemController.java
+++ rpc-demo/rpc-demo-server/src/main/java/org/infinity/rpc/demoserver/controller/SystemController.java
@@ -2,12 +2,25 @@ package org.infinity.rpc.demoserver.controller;
 
 import io.swagger.annotations.ApiOperation;
 import org.infinity.rpc.demoserver.utils.NetworkUtils;
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RestController
 public class SystemController {
+
+    @Resource
+    private ApplicationContext applicationContext;
+
+    @ApiOperation("get bean")
+    @GetMapping("/api/system/bean")
+    public ResponseEntity<Object> getBean(@RequestParam(value = "name") String name) {
+        return ResponseEntity.ok(applicationContext.getBean(name));
+    }
 
     @ApiOperation("get internet IP")
     @GetMapping("/api/system/internet-ip")
--- rpc-web-center/src/main/java/org/infinity/rpc/webcenter/controller/SystemController.java
+++ rpc-web-center/src/main/java/org/infinity/rpc/webcenter/controller/SystemController.java
@@ -2,26 +2,36 @@ package org.infinity.rpc.webcenter.controller;
 
 import io.changock.runner.core.ChangockBase;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.infinity.rpc.webcenter.domain.Authority;
 import org.infinity.rpc.webcenter.utils.NetworkUtils;
+import org.springframework.context.ApplicationContext;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RestController
 @Api(tags = "系统")
 @Slf4j
 public class SystemController {
 
-    private final ChangockBase  changockBase;
-    private final MongoTemplate mongoTemplate;
+    @Resource
+    private ApplicationContext applicationContext;
+    @Resource
+    private ChangockBase       changockBase;
+    @Resource
+    private MongoTemplate      mongoTemplate;
 
-    public SystemController(ChangockBase changockBase, MongoTemplate mongoTemplate) {
-        this.changockBase = changockBase;
-        this.mongoTemplate = mongoTemplate;
+    @ApiOperation("get bean")
+    @GetMapping("/api/system/bean")
+    public ResponseEntity<Object> getBean(@RequestParam(value = "name") String name) {
+        return ResponseEntity.ok(applicationContext.getBean(name));
     }
 
     @GetMapping("/api/system/internet-ip")
