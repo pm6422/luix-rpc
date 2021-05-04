1810ab4b1d921b9588cd7a44da619f0e5a6424dd
刘尧 <liuyao@xforceplus.com>
2021-05-04 19:01:02 +0800

    fixed menuTree hessian stackoverflow serialization issue

--- rpc-demo/rpc-demo-common/src/main/java/org/infinity/rpc/democommon/dto/AdminMenuTreeDTO.java
+++ rpc-demo/rpc-demo-common/src/main/java/org/infinity/rpc/democommon/dto/AdminMenuTreeDTO.java
@@ -1,20 +1,24 @@
 package org.infinity.rpc.democommon.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
-import org.infinity.rpc.democommon.domain.AdminMenu;
-import org.infinity.rpc.democommon.domain.base.BaseAdminMenu;
 
 import java.io.Serializable;
 import java.util.List;
 
 @Data
-@EqualsAndHashCode(callSuper = true)
-@ToString(callSuper = true)
-public class AdminMenuTreeDTO extends BaseAdminMenu implements Serializable {
+public class AdminMenuTreeDTO implements Serializable {
 
-    private static final long                   serialVersionUID = -3123723565571697648L;
-    private              List<AdminMenuTreeDTO> children;
+    private static final long    serialVersionUID = -3123723565571697648L;
+    @ApiModelProperty(value = "ID", required = true)
+    private transient    String  id;
+    @ApiModelProperty(value = "名称", required = true)
+    private              String  name;
+    @ApiModelProperty(value = "链接地址", required = true)
+    private              String  url;
+    @ApiModelProperty(value = "排序序号", required = true)
+    private transient    Integer sequence;
+
+    private List<AdminMenuTreeDTO> children;
 }
 
