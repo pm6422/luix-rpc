51da57c808c5b7767fc59d7e88bea4249d73312b
刘尧 <liuyao@xforceplus.com>
2021-05-06 17:00:54 +0800

    refactor build-in method

--- rpc-core/src/main/java/org/infinity/rpc/core/config/impl/ProviderConfig.java
+++ rpc-core/src/main/java/org/infinity/rpc/core/config/impl/ProviderConfig.java
@@ -5,15 +5,16 @@ import lombok.Data;
 @Data
 public class ProviderConfig extends ServiceConfig {
     // Build-in methods
-    public static final String  METHOD_HEALTH           = "$health";
-    public static final String  METHOD_META             = "$methodMeta";
-    public static final String  METHOD_APPLICATION_META = "$applicationMeta";
-    public static final String  METHOD_SYSTEM_TIME      = "$systemTime";
-    public static final String  PREFIX                  = "provider";
+    public static final String METHOD_HEALTH           = "$health";
+    public static final String METHOD_META             = "$methodMeta";
+    public static final String METHOD_APPLICATION_META = "$applicationMeta";
+    public static final String METHOD_SYSTEM_TIME      = "$systemTime";
+
+    public static final String  PREFIX     = "provider";
     /**
      * Indicates whether all the providers were exposed to registry automatically
      */
-    private             boolean autoExpose              = true;
+    private             boolean autoExpose = true;
 
     public void init() {
     }
