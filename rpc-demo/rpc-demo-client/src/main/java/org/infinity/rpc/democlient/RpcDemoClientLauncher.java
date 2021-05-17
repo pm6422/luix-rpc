8783825331e8eba72f41154bbfe9109d7ee07d84
刘尧 <liuyao@xforceplus.com>
2021-05-17 19:00:48 +0800

    add serializer perf tests

--- rpc-demo/rpc-demo-client/src/main/java/org/infinity/rpc/democlient/RpcDemoClientLauncher.java
+++ rpc-demo/rpc-demo-client/src/main/java/org/infinity/rpc/democlient/RpcDemoClientLauncher.java
@@ -2,19 +2,13 @@ package org.infinity.rpc.democlient;
 
 import com.dtflys.forest.springboot.annotation.ForestScan;
 import lombok.extern.slf4j.Slf4j;
-import org.infinity.rpc.utilities.serializer.kryo.KryoUtils;
 import org.infinity.rpc.spring.boot.EnableRpc;
-import org.infinity.rpc.spring.enhancement.kryo.serializer.PageImplSerializer;
-import org.infinity.rpc.spring.enhancement.kryo.serializer.PageRequestSerializer;
-import org.infinity.rpc.spring.enhancement.kryo.serializer.PageableSerializer;
-import org.infinity.rpc.spring.enhancement.kryo.serializer.SortSerializer;
+import org.infinity.rpc.spring.enhancement.kryo.serializer.*;
+import org.infinity.rpc.utilities.serializer.kryo.KryoUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 
 import javax.annotation.PostConstruct;
 
@@ -24,7 +18,7 @@ import javax.annotation.PostConstruct;
 @EnableRpc
 @ForestScan(basePackages = "org.infinity.rpc.democlient.restservice")
 public class RpcDemoClientLauncher {
-    
+
     /**
      * Entrance method which used to run the application. Spring profiles can be configured with a program arguments
      * --spring.profiles.active=your-active-profile
@@ -41,5 +35,6 @@ public class RpcDemoClientLauncher {
         KryoUtils.registerClass(PageRequest.class, new PageRequestSerializer());
         KryoUtils.registerClass(Pageable.class, new PageableSerializer());
         KryoUtils.registerClass(PageImpl.class, new PageImplSerializer());
+        KryoUtils.registerClass(Page.class, new PageSerializer());
     }
 }
---
+++ rpc-demo/rpc-demo-client/src/test/java/org/infinity/rpc/appclient/serializer/service/AppServiceImpl.java
@@ -0,0 +1,42 @@
+package org.infinity.rpc.appclient.serializer.service;
+
+import org.infinity.rpc.core.server.response.FutureResponse;
+import org.infinity.rpc.democommon.domain.App;
+import org.infinity.rpc.democommon.service.AppService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+public class AppServiceImpl implements AppService {
+    @Override
+    public Page<App> findAll(Pageable pageable) {
+        List<App> list = new ArrayList<>(100);
+        IntStream.range(0, 100).forEach(i -> list.add(new App("testApp", true)));
+        return new PageImpl<>(list, pageable, 100);
+    }
+
+    @Override
+    public Optional<App> findById(String id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public FutureResponse insert(App domain) {
+        return null;
+    }
+
+    @Override
+    public void update(App domain) {
+
+    }
+
+    @Override
+    public void deleteById(String id) {
+
+    }
+}
