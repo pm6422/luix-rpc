483830c2d41e922ddfbe836e81b79eece5e6750b
刘尧 <liuyao@xforceplus.com>
2021-06-03 13:38:03 +0800

    refactor

--- rpc-core/src/main/java/org/infinity/rpc/core/thread/ScheduledThreadPool.java
+++ rpc-core/src/main/java/org/infinity/rpc/core/thread/ScheduledThreadPool.java
@@ -30,7 +30,7 @@ public class ScheduledThreadPool {
         THREAD_POOL_MAP.put(DESTROY_SENDER_THREAD_POOL, Executors.newScheduledThreadPool(1));
         THREAD_POOL_MAP.put(DESTROY_NETTY_TIMEOUT_TASK_THREAD_POOL, Executors.newScheduledThreadPool(1));
 
-        // Clean up the thread pools when the system exits
+        // Destroy the thread pools when the system exits
         ShutdownHook.add(() -> {
             for (ScheduledExecutorService threadPool : THREAD_POOL_MAP.values()) {
                 if (!threadPool.isShutdown()) {
@@ -40,29 +40,62 @@ public class ScheduledThreadPool {
         });
     }
 
+    /**
+     * Schedule a task to run at periodic intervals
+     *
+     * @param threadPoolName thread pool name
+     * @param interval       execution interval
+     * @param task           task to be executed
+     * @return result
+     */
     public static ScheduledFuture<?> schedulePeriodicalTask(String threadPoolName, long interval, Runnable task) {
         return schedulePeriodicalTask(threadPoolName, interval, interval, TimeUnit.MILLISECONDS, task);
     }
 
+    /**
+     * Schedule a task to run at periodic intervals
+     *
+     * @param threadPoolName thread pool name
+     * @param initialDelay   initial delay
+     * @param interval       execution interval
+     * @param timeUnit       time unit
+     * @param task           task to be executed
+     * @return result
+     */
     public static ScheduledFuture<?> schedulePeriodicalTask(String threadPoolName, long initialDelay,
                                                             long interval, TimeUnit timeUnit, Runnable task) {
         Validate.isTrue(THREAD_POOL_MAP.containsKey(threadPoolName), "Please specify a valid thread pool name!");
 
-        // Schedule a task to run at periodic intervals
         // 以上一个任务开始的时间计时，period时间过去后，检测上一个任务是否执行完毕，
         // 如果上一个任务执行完毕，则当前任务立即执行，如果上一个任务没有执行完毕，则需要等上一个任务执行完毕后立即执行。
         return THREAD_POOL_MAP.get(threadPoolName).scheduleAtFixedRate(task, initialDelay, interval, timeUnit);
     }
 
+    /**
+     * Execute once after a daley time
+     *
+     * @param threadPoolName thread pool name
+     * @param delay          delay time in milliseconds
+     * @param task           task to be executed
+     * @return thread pool
+     */
     public static ScheduledExecutorService scheduleDelayTask(String threadPoolName, long delay, Runnable task) {
         return scheduleDelayTask(threadPoolName, delay, TimeUnit.MILLISECONDS, task);
     }
 
+    /**
+     * Execute once after a daley time
+     *
+     * @param threadPoolName thread pool name
+     * @param delay          delay time in milliseconds
+     * @param timeUnit       time unit
+     * @param task           task to be executed
+     * @return thread pool
+     */
     public static ScheduledExecutorService scheduleDelayTask(String threadPoolName, long delay,
                                                              TimeUnit timeUnit, Runnable task) {
         Validate.isTrue(THREAD_POOL_MAP.containsKey(threadPoolName), "Please specify a valid thread pool name!");
 
-        // Execute once after a daley time
         ScheduledExecutorService scheduledExecutorService = THREAD_POOL_MAP.get(threadPoolName);
         scheduledExecutorService.schedule(task, delay, timeUnit);
         return scheduledExecutorService;
