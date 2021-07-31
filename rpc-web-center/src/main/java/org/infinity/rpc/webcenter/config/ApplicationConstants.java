5b8367a00267fb172ad8bd08adeec043dab2d0d3
刘尧 <liuyao@xforceplus.com>
2021-07-31 20:33:05 +0800

    web center UI

--- rpc-web-center/src/main/java/org/infinity/rpc/webcenter/config/ApplicationConstants.java
+++ rpc-web-center/src/main/java/org/infinity/rpc/webcenter/config/ApplicationConstants.java
@@ -10,6 +10,6 @@ public final class ApplicationConstants {
     public static final String   SPRING_PROFILE_TEST = "test";
     public static final String   SPRING_PROFILE_PROD = "prod";
     public static final String[] AVAILABLE_PROFILES  = new String[]{SPRING_PROFILE_TEST, SPRING_PROFILE_PROD};
-    public static final Locale   SYSTEM_LOCALE       = Locale.SIMPLIFIED_CHINESE;
+    public static final Locale   SYSTEM_LOCALE       = Locale.US;
     public static final String   DEFAULT_REG         = "zookeeper://localhost:2181/registry";
 }
