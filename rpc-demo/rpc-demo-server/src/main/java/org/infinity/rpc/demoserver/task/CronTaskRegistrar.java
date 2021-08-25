c94d3c653626edcf0f382e832f65b6f140a5d2d3
刘尧 <liuyao@xforceplus.com>
2021-08-25 17:25:27 +0800

    refactor task

--- rpc-demo/rpc-demo-server/src/main/java/org/infinity/rpc/demoserver/task/CronTaskRegistrar.java
+++ rpc-demo/rpc-demo-server/src/main/java/org/infinity/rpc/demoserver/task/CronTaskRegistrar.java
@@ -15,19 +15,19 @@ import java.util.concurrent.ScheduledFuture;
 @Component
 public class CronTaskRegistrar implements DisposableBean {
 
-    private final Map<Runnable, ScheduledTask> scheduledTasks = new ConcurrentHashMap<>(16);
+    private static final Map<Runnable, ScheduledTask> SCHEDULED_TASKS = new ConcurrentHashMap<>(16);
     /**
      * Spring task scheduler
      */
     @Resource
-    private       TaskScheduler                taskScheduler;
+    private              TaskScheduler                taskScheduler;
 
     public void addCronTask(Runnable task, String cronExpression) {
         Validate.notNull(task, "Task must NOT be null!");
         Validate.notEmpty(cronExpression, "Cron expression must NOT be empty!");
 
         CronTask cronTask = new CronTask(task, cronExpression);
-        this.scheduledTasks.put(task, createCronTask(cronTask));
+        this.SCHEDULED_TASKS.put(task, createCronTask(cronTask));
     }
 
     private ScheduledTask createCronTask(CronTask cronTask) {
@@ -38,7 +38,7 @@ public class CronTaskRegistrar implements DisposableBean {
     public void addFixedRateTask(Runnable task, long interval) {
         Validate.notNull(task, "Task must NOT be null!");
 
-        this.scheduledTasks.put(task, createFixedRateTask(task, interval));
+        this.SCHEDULED_TASKS.put(task, createFixedRateTask(task, interval));
     }
 
     private ScheduledTask createFixedRateTask(Runnable task, long period) {
@@ -49,7 +49,7 @@ public class CronTaskRegistrar implements DisposableBean {
     public void removeTask(Runnable task) {
         Validate.notNull(task, "Task must NOT be null!");
 
-        ScheduledTask scheduledTask = this.scheduledTasks.remove(task);
+        ScheduledTask scheduledTask = this.SCHEDULED_TASKS.remove(task);
         if (scheduledTask != null) {
             scheduledTask.cancel();
         }
@@ -57,10 +57,10 @@ public class CronTaskRegistrar implements DisposableBean {
 
     @Override
     public void destroy() {
-        if (MapUtils.isEmpty(scheduledTasks)) {
+        if (MapUtils.isEmpty(SCHEDULED_TASKS)) {
             return;
         }
-        scheduledTasks.values().forEach(ScheduledTask::cancel);
-        scheduledTasks.clear();
+        SCHEDULED_TASKS.values().forEach(ScheduledTask::cancel);
+        SCHEDULED_TASKS.clear();
     }
 }
