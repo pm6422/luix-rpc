7791ab21adf7ed5d17c6f7fe23829c72b539d2d8
刘尧 <liuyao@xforceplus.com>
2021-09-03 18:07:10 +0800

    add statistics

---
+++ rpc-web-center/src/main/java/org/infinity/rpc/webcenter/controller/RpcStatisticsController.java
@@ -0,0 +1,50 @@
+package org.infinity.rpc.webcenter.controller;
+
+import com.codahale.metrics.annotation.Timed;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.infinity.rpc.webcenter.dto.StatisticsDTO;
+import org.infinity.rpc.webcenter.repository.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class RpcStatisticsController {
+
+    @Resource
+    private RpcApplicationRepository          rpcApplicationRepository;
+    @Resource
+    private RpcServerRepository               rpcServerRepository;
+    @Resource
+    private RpcServiceRepository              rpcServiceRepository;
+    @Resource
+    private RpcProviderRepository             rpcProviderRepository;
+    @Resource
+    private RpcConsumerRepository             rpcConsumerRepository;
+    @Resource
+    private RpcScheduledTaskRepository        rpcScheduledTaskRepository;
+    @Resource
+    private RpcScheduledTaskHistoryRepository rpcScheduledTaskHistoryRepository;
+
+
+    @ApiOperation("get RPC statistics data")
+    @GetMapping("api/rpc-statistics/data")
+    @Timed
+    public ResponseEntity<StatisticsDTO> getStatistics() {
+        StatisticsDTO dto = StatisticsDTO.builder()
+                .applicationCount(rpcApplicationRepository.count())
+                .serverCount(rpcServerRepository.count())
+                .serviceCount(rpcServiceRepository.count())
+                .providerCount(rpcProviderRepository.count())
+                .consumerCount(rpcConsumerRepository.count())
+                .taskCount(rpcScheduledTaskRepository.count())
+                .taskExecutedCount(rpcScheduledTaskHistoryRepository.count())
+                .build();
+
+        return ResponseEntity.ok(dto);
+    }
+}
---
+++ rpc-web-center/src/main/java/org/infinity/rpc/webcenter/dto/StatisticsDTO.java
@@ -0,0 +1,30 @@
+package org.infinity.rpc.webcenter.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class StatisticsDTO {
+    private long applicationCount;
+    private long activeApplicationCount;
+
+    private long serverCount;
+    private long activeServerCount;
+
+    private long serviceCount;
+    private long activeServiceCount;
+
+    private long providerCount;
+    private long activeProviderCount;
+
+    private long consumerCount;
+    private long activeConsumerCount;
+
+    private long taskCount;
+    private long activeTaskCount;
+    private long taskExecutionCount;
+    private long dailyTaskExecutionCount;
+    private long taskExecutedCount;
+    private long taskFailedCount;
+}
