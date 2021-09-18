e4426e15b1863cd7ddb91e5c61d1d9ed64c5244f
刘尧 <liuyao@xforceplus.com>
2021-09-18 15:54:27 +0800

    add test methods

--- luix-demo/luix-demo-common/src/main/java/org/infinity/luix/democommon/service/UserService.java
+++ luix-demo/luix-demo-common/src/main/java/org/infinity/luix/democommon/service/UserService.java
@@ -2,7 +2,19 @@ package org.infinity.luix.democommon.service;
 
 import org.infinity.luix.democommon.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findOneByLogin(String login);
+
+    User findOneByMobileNo(Integer mobileNo);
+
+    User findOneByMobile(int mobileNo);
+
+    List<User> findByWeight(Double weight);
+
+    List<User> findByEnabled(Boolean enabled);
+
+    boolean save(User user);
 }
--- luix-demo/luix-demo-server/src/main/java/org/infinity/luix/demoserver/service/impl/UserServiceImpl.java
+++ luix-demo/luix-demo-server/src/main/java/org/infinity/luix/demoserver/service/impl/UserServiceImpl.java
@@ -5,9 +5,12 @@ import org.infinity.luix.core.server.annotation.RpcProvider;
 import org.infinity.luix.democommon.domain.User;
 import org.infinity.luix.democommon.service.UserService;
 import org.infinity.luix.demoserver.repository.UserRepository;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.util.Assert;
 
 import javax.annotation.Resource;
+import java.util.List;
 import java.util.Locale;
 
 @RpcProvider
@@ -22,5 +25,38 @@ public class UserServiceImpl implements UserService {
         Assert.hasText(login, "it must not be null, empty, or blank");
         return userRepository.findOneByUserNameOrEmailOrMobileNo(login.toLowerCase(Locale.ENGLISH),
                 login.toLowerCase(Locale.ENGLISH), login.toLowerCase(Locale.ENGLISH)).orElse(null);
+    }
+
+    @Override
+    public User findOneByMobileNo(Integer mobileNo) {
+        return userRepository.findOneByUserNameOrEmailOrMobileNo(mobileNo.toString(),
+                mobileNo.toString(), mobileNo.toString()).orElse(null);
+    }
+
+    @Override
+    public User findOneByMobile(int mobileNo) {
+        log.info("mobileNo: {}", mobileNo);
+        return null;
+    }
+
+    @Override
+    public List<User> findByWeight(Double weight) {
+        return null;
+    }
+
+    @Override
+    public List<User> findByEnabled(Boolean enabled) {
+        // Ignore query parameter if it has a null value
+        ExampleMatcher matcher = ExampleMatcher.matching().withIgnoreNullValues();
+        User user = new User();
+        user.setEnabled(enabled);
+        Example<User> queryExample = Example.of(user, matcher);
+        return userRepository.findAll(queryExample);
+    }
+
+    @Override
+    public boolean save(User user) {
+        userRepository.save(user);
+        return true;
     }
 }
