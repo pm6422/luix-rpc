72dcee167950d8aa717c894837f6d2d7715630a1
刘尧 <liuyao@xforceplus.com>
2021-09-29 18:49:44 +0800

    refactor

--- luix-web-center/src/main/java/org/infinity/luix/webcenter/service/impl/RpcStatisticsServiceImpl.java
+++ luix-web-center/src/main/java/org/infinity/luix/webcenter/service/impl/RpcStatisticsServiceImpl.java
@@ -8,6 +8,11 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.stream.Collectors;
 
 @Service
 public class RpcStatisticsServiceImpl implements RpcStatisticsService {
@@ -26,18 +31,40 @@ public class RpcStatisticsServiceImpl implements RpcStatisticsService {
     private RpcScheduledTaskRepository        rpcScheduledTaskRepository;
     @Resource
     private RpcScheduledTaskHistoryRepository rpcScheduledTaskHistoryRepository;
+    @Resource
+    private Executor                          asyncTaskExecutor;
 
+    /**
+     * Refer to https://www.toutiao.com/a6783214804937998851/
+     *
+     * @param taskId task ID
+     */
     @Override
     @Async
     public void getStatistics(String taskId) {
+        List<CompletableFuture<Long>> futures = new ArrayList<>(7);
+        futures.add(CompletableFuture.supplyAsync(rpcApplicationRepository::count, asyncTaskExecutor));
+        futures.add(CompletableFuture.supplyAsync(rpcServerRepository::count, asyncTaskExecutor));
+        futures.add(CompletableFuture.supplyAsync(rpcServiceRepository::count, asyncTaskExecutor));
+        futures.add(CompletableFuture.supplyAsync(rpcProviderRepository::count, asyncTaskExecutor));
+        futures.add(CompletableFuture.supplyAsync(rpcConsumerRepository::count, asyncTaskExecutor));
+        futures.add(CompletableFuture.supplyAsync(rpcScheduledTaskRepository::count, asyncTaskExecutor));
+        futures.add(CompletableFuture.supplyAsync(rpcScheduledTaskHistoryRepository::count, asyncTaskExecutor));
+
+        CompletableFuture<Void> allFutures = CompletableFuture.allOf(futures.toArray(new CompletableFuture[7]));
+        CompletableFuture<List<Long>> listCompletableFuture = allFutures
+                .thenApplyAsync(val -> futures.stream().map(CompletableFuture::join)
+                        .collect(Collectors.toList()), asyncTaskExecutor);
+        List<Long> results = listCompletableFuture.join();
+
         StatisticsDTO dto = StatisticsDTO.builder()
-                .applicationCount(rpcApplicationRepository.count())
-                .serverCount(rpcServerRepository.count())
-                .serviceCount(rpcServiceRepository.count())
-                .providerCount(rpcProviderRepository.count())
-                .consumerCount(rpcConsumerRepository.count())
-                .taskCount(rpcScheduledTaskRepository.count())
-                .taskExecutedCount(rpcScheduledTaskHistoryRepository.count())
+                .applicationCount(results.get(0))
+                .serverCount(results.get(1))
+                .serviceCount(results.get(2))
+                .providerCount(results.get(3))
+                .consumerCount(results.get(4))
+                .taskCount(results.get(5))
+                .taskExecutedCount(results.get(6))
                 .build();
 
         StatisticsResultQueue.put(taskId, dto);
