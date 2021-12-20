9b6cf94cbbc45a2a316649cd6ea45f3b22020297
刘尧 <liuyao@xforceplus.com>
2021-12-20 17:11:55 +0800

    schedule reset

--- luix-demo/luix-demo-server/src/main/java/org/infinity/luix/demoserver/controller/SystemController.java
+++ luix-demo/luix-demo-server/src/main/java/org/infinity/luix/demoserver/controller/SystemController.java
@@ -66,12 +66,13 @@ public class SystemController {
     @ApiOperation("reset database")
     @GetMapping("/open-api/systems/reset-database")
     public String resetDatabase() {
-        mongoTemplate.getDb().drop();
-        changockBase.execute();
+        reset();
         return "Reset database successfully.";
     }
 
     @Scheduled(cron = "0 0/5 * * * ?")
     public void reset() {
+        mongoTemplate.getDb().drop();
+        changockBase.execute();
     }
 }
