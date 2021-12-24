091486dfdaba74e6701d97070696f3f9a9292a6c
刘尧 <liuyao@xforceplus.com>
2021-12-24 20:19:42 +0800

    change embedded mongo data dir

--- luix-demo/luix-demo-server/src/main/docker/jib/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "The application will start in ${SLEEP_TIME}s..." && sleep ${SLEEP_TIME}
-exec USER root
-exec mkdir -p /app/embedmongo
-exec chmod 755 /app/embedmongo
-exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Duser.home=/app -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "${START_CLASS}" "$@"
