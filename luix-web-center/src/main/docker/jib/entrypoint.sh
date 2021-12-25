6896efaafb335f72720ca939a1c9b432cbb3b275
刘尧 <liuyao@xforceplus.com>
2021-12-25 19:56:01 +0800

    add dev profile

--- luix-web-center/src/main/docker/jib/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "The application will start in ${SLEEP_TIME}s..." && sleep ${SLEEP_TIME}
-exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "${START_CLASS}" "$@"
