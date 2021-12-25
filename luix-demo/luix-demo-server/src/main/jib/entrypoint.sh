b5be34f8ca98b7a520bb684d8d9f560fb5bfd570
刘尧 <liuyao@xforceplus.com>
2021-12-25 11:39:12 +0800

    refactor

--- luix-demo/luix-demo-server/src/main/jib/entrypoint.sh
+++ luix-demo/luix-demo-server/src/main/jib/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "The application will start in ${SLEEP_TIME}s..." && sleep ${SLEEP_TIME}
-exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Duser.home=/tmp -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "${START_CLASS}" "$@"
+exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "${START_CLASS}" "$@"
