b6841747d3a705ae0f69a19d8bfe3b12c865ed0e
刘尧 <liuyao@xforceplus.com>
2021-12-30 14:29:16 +0800

    refactor system info

--- luix-demo/luix-demo-client/src/main/webapp/app/constants.js
+++ luix-demo/luix-demo-client/src/main/webapp/app/constants.js
@@ -5,11 +5,8 @@
     angular
         .module('smartcloudserviceApp')
         .constant('APP_NAME', 'luix-demo-client')
-        .constant('VERSION', '1.0.0')
-        .constant('COMPANY_NAME', 'Infinity Organization')
         .constant('PAGINATION_CONSTANTS', {
             'itemsPerPage': 10
          })
-        //        .constant('ENV', 'dev')
         .constant('DEBUG_INFO_ENABLED', true);
 })();
