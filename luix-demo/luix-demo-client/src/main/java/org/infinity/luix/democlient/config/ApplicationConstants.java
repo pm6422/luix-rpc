b1445e8fc56725e2e87ce4e6f9f243957ae608dc
刘尧 <liuyao@xforceplus.com>
2021-12-30 22:52:01 +0800

    refactor system info

--- luix-demo/luix-demo-client/src/main/java/org/infinity/luix/democlient/config/ApplicationConstants.java
+++ luix-demo/luix-demo-client/src/main/java/org/infinity/luix/democlient/config/ApplicationConstants.java
@@ -11,5 +11,5 @@ public final class ApplicationConstants {
     public static final String   SPRING_PROFILE_DEMO = "demo";
     public static final String   SPRING_PROFILE_PROD = "prod";
     public static final String[] AVAILABLE_PROFILES  = new String[]{SPRING_PROFILE_DEV, SPRING_PROFILE_DEMO, SPRING_PROFILE_PROD};
-    public static final Locale   SYSTEM_LOCALE       = Locale.SIMPLIFIED_CHINESE;
+    public static final Locale   SYSTEM_LOCALE       = Locale.US;
 }
