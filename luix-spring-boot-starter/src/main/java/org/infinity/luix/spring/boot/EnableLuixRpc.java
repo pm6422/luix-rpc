efa6d3dd63257229f13ec8277cea52ea984d69e0
刘尧 <liuyao@xforceplus.com>
2022-01-17 13:59:10 +0800

    refactor

--- luix-spring-boot-starter/src/main/java/org/infinity/luix/spring/boot/EnableLuixRpc.java
+++ luix-spring-boot-starter/src/main/java/org/infinity/luix/spring/boot/EnableLuixRpc.java
@@ -2,13 +2,11 @@ package org.infinity.luix.spring.boot;
 
 import org.springframework.context.annotation.Import;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
+@Documented
 @Import(RpcServiceScanRegistrar.class)
 public @interface EnableLuixRpc {
 
