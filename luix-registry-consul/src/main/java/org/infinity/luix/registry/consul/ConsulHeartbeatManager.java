27b8b88bc51d910c3d1b2bf2b9d32273332a0ac7
刘尧 <liuyao@xforceplus.com>
2022-01-26 15:23:07 +0800

    refactor

--- luix-registry-consul/src/main/java/org/infinity/luix/registry/consul/ConsulHeartbeatManager.java
+++ luix-registry-consul/src/main/java/org/infinity/luix/registry/consul/ConsulHeartbeatManager.java
@@ -9,8 +9,8 @@ import java.util.concurrent.*;
 import static org.infinity.luix.registry.consul.ConsulService.TTL;
 
 /**
- * consul 心跳管理类。 rpc服务把需要设置passing状态的serviceid注册到此类，
- * 此类会定时对注册的serviceid设置passing状态（实际是对servcieid对应对checkid设置passing状态），
+ * consul 心跳管理类。 rpc服务把需要设置passing状态的serviceId注册到此类，
+ * 此类会定时对注册的serviceId设置passing状态（实际是对serviceId对应对checkid设置passing状态），
  * 从而完成servivce的心跳。
  * 开关开启后会进行心跳，开关关闭则停止心跳。
  */
@@ -19,64 +19,57 @@ public class ConsulHeartbeatManager {
     /**
      * 连续检测开关变更的最大次数，超过这个次数就发送一次心跳
      */
-    public static int MAX_SWITCHER_CHECK_TIMES = 10;
+    public static    int                       MAX_SWITCHER_CHECK_TIMES       = 10;
     /**
      * 心跳周期，取ttl的2/3
      */
-    public static int HEARTBEAT_CIRCLE         = (TTL * 1000 * 2) / 3;
+    public static    int                       HEARTBEAT_CIRCLE               = (TTL * 1000 * 2) / 3;
     /**
      * 检测开关变更的频率，连续检测MAX_SWITCHER_CHECK_TIMES次必须发送一次心跳。
      */
-    public static int SWITCHER_CHECK_CIRCLE    = HEARTBEAT_CIRCLE / MAX_SWITCHER_CHECK_TIMES;
-
-    private AbstractConsulClient      client;
-    // 所有需要进行心跳的serviceid.
-    private ConcurrentHashSet<String> serviceIds = new ConcurrentHashSet<String>();
-
-    private          ThreadPoolExecutor       jobExecutor;
-    private          ScheduledExecutorService heartbeatExecutor;
+    public static    int                       SWITCHER_CHECK_CIRCLE          = HEARTBEAT_CIRCLE / MAX_SWITCHER_CHECK_TIMES;
+    private          AbstractConsulClient      client;
+    // 所有需要进行心跳的serviceId.
+    private final    ConcurrentHashSet<String> serviceIds                     = new ConcurrentHashSet<>();
+    private final    ThreadPoolExecutor        jobExecutor;
+    private final    ScheduledExecutorService  heartbeatExecutor;
     // 上一次心跳开关的状态
-    private          boolean                  lastHeartBeatSwitcherStatus    = false;
-    private volatile boolean                  currentHeartBeatSwitcherStatus = false;
+    private          boolean                   lastHeartBeatSwitcherStatus    = false;
+    private volatile boolean                   currentHeartBeatSwitcherStatus = false;
     // 开关检查次数。
-    private          int                      switcherCheckTimes             = 0;
+    private          int                       switcherCheckTimes             = 0;
 
     public ConsulHeartbeatManager(AbstractConsulClient client) {
         this.client = client;
         heartbeatExecutor = Executors.newSingleThreadScheduledExecutor();
-        ArrayBlockingQueue<Runnable> workQueue = new ArrayBlockingQueue<Runnable>(
-                10000);
-        jobExecutor = new ThreadPoolExecutor(5, 30, 30 * 1000,
-                TimeUnit.MILLISECONDS, workQueue);
+        ArrayBlockingQueue<Runnable> workQueue = new ArrayBlockingQueue<>(10000);
+        jobExecutor = new ThreadPoolExecutor(5, 30, 30 * 1000, TimeUnit.MILLISECONDS, workQueue);
     }
 
     public void start() {
         heartbeatExecutor.scheduleAtFixedRate(
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        // 由于consul的check set pass会导致consul
-                        // server的写磁盘操作，过于频繁的心跳会导致consul
-                        // 性能问题，只能将心跳方式改为较长的周期进行一次探测。又因为想在关闭心跳开关后尽快感知
-                        // 就将心跳改为以较小周期检测心跳开关是否变动，连续检测多次后给consul server发送一次心跳。
-                        // TODO 改为开关listener方式。
-                        try {
-                            boolean switcherStatus = isHeartbeatOpen();
-                            if (isSwitcherChange(switcherStatus)) { // 心跳开关状态变更
-                                processHeartbeat(switcherStatus);
-                            } else {// 心跳开关状态未变更
-                                if (switcherStatus) {// 开关为开启状态，则连续检测超过MAX_SWITCHER_CHECK_TIMES次发送一次心跳
-                                    switcherCheckTimes++;
-                                    if (switcherCheckTimes >= MAX_SWITCHER_CHECK_TIMES) {
-                                        processHeartbeat(true);
-                                        switcherCheckTimes = 0;
-                                    }
+                () -> {
+                    // 由于consul的check set pass会导致consul
+                    // server的写磁盘操作，过于频繁的心跳会导致consul
+                    // 性能问题，只能将心跳方式改为较长的周期进行一次探测。又因为想在关闭心跳开关后尽快感知
+                    // 就将心跳改为以较小周期检测心跳开关是否变动，连续检测多次后给consul server发送一次心跳。
+                    // TODO 改为开关listener方式。
+                    try {
+                        boolean switcherStatus = isHeartbeatOpen();
+                        if (isSwitcherChange(switcherStatus)) { // 心跳开关状态变更
+                            processHeartbeat(switcherStatus);
+                        } else {// 心跳开关状态未变更
+                            if (switcherStatus) {// 开关为开启状态，则连续检测超过MAX_SWITCHER_CHECK_TIMES次发送一次心跳
+                                switcherCheckTimes++;
+                                if (switcherCheckTimes >= MAX_SWITCHER_CHECK_TIMES) {
+                                    processHeartbeat(true);
+                                    switcherCheckTimes = 0;
                                 }
                             }
-
-                        } catch (Exception e) {
-                            log.error("consul heartbeat executor err:", e);
                         }
+
+                    } catch (Exception e) {
+                        log.error("consul heartbeat executor err:", e);
                     }
                 }, SWITCHER_CHECK_CIRCLE,
                 SWITCHER_CHECK_CIRCLE, TimeUnit.MILLISECONDS);
@@ -99,11 +92,11 @@ public class ConsulHeartbeatManager {
     }
 
     protected void processHeartbeat(boolean isPass) {
-        for (String serviceid : serviceIds) {
+        for (String serviceId : serviceIds) {
             try {
-                jobExecutor.execute(new HeartbeatJob(serviceid, isPass));
+                jobExecutor.execute(new HeartbeatJob(serviceId, isPass));
             } catch (RejectedExecutionException ree) {
-                log.error("execute heartbeat job fail! serviceid:" + serviceid + " is rejected");
+                log.error("execute heartbeat job fail! serviceId:" + serviceId + " is rejected");
             }
         }
     }
@@ -115,21 +108,21 @@ public class ConsulHeartbeatManager {
     }
 
     /**
-     * 添加consul serviceid，添加后的serviceid会通过定时设置passing状态保持心跳。
+     * 添加consul serviceId，添加后的serviceId会通过定时设置passing状态保持心跳。
      *
-     * @param serviceid
+     * @param serviceId
      */
-    public void addHeartbeatServcieId(String serviceid) {
-        serviceIds.add(serviceid);
+    public void addHeartbeatServcieId(String serviceId) {
+        serviceIds.add(serviceId);
     }
 
     /**
-     * 移除serviceid，对应的serviceid不会在进行心跳。
+     * 移除serviceId，对应的serviceId不会在进行心跳。
      *
-     * @param serviceid
+     * @param serviceId
      */
-    public void removeHeartbeatServiceId(String serviceid) {
-        serviceIds.remove(serviceid);
+    public void removeHeartbeatServiceId(String serviceId) {
+        serviceIds.remove(serviceId);
     }
 
     // 检查心跳开关是否打开
@@ -142,8 +135,8 @@ public class ConsulHeartbeatManager {
     }
 
     class HeartbeatJob implements Runnable {
-        private String  serviceId;
-        private boolean isPass;
+        private final String  serviceId;
+        private final boolean isPass;
 
         public HeartbeatJob(String serviceId, boolean isPass) {
             super();
