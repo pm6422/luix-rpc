cdf9240c4559ee0838c4a673a568c8a9a7f96361
刘尧 <liuyao@xforceplus.com>
2022-01-28 15:25:16 +0800

    refactor meter

--- luix-web-center/src/main/java/org/infinity/luix/webcenter/service/impl/SecurityErrorMeterServiceImpl.java
+++ luix-web-center/src/main/java/org/infinity/luix/webcenter/service/impl/SecurityErrorMeterServiceImpl.java
@@ -8,15 +8,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class SecurityErrorMeterServiceImpl implements SecurityErrorMeterService {
 
-    public static final String INVALID_TOKENS_METER_NAME            = "security.authentication.invalid-tokens";
-    public static final String INVALID_TOKENS_METER_BASE_UNIT       = "errors";
-    public static final String INVALID_TOKENS_METER_DESCRIPTION     = "Indicates validation error count of the tokens presented by the clients.";
-    public static final String INVALID_TOKENS_METER_CAUSE_DIMENSION = "cause";
-
-    private final Counter tokenInvalidSignatureCounter;
-    private final Counter tokenExpiredCounter;
-    private final Counter tokenUnsupportedCounter;
-    private final Counter tokenMalformedCounter;
+    private static final String  INVALID_TOKENS_METER_NAME            = "security.authentication.invalid-tokens";
+    private static final String  INVALID_TOKENS_METER_BASE_UNIT       = "errors";
+    private static final String  INVALID_TOKENS_METER_DESCRIPTION     = "Indicates validation error count of the tokens presented by the clients.";
+    private static final String  INVALID_TOKENS_METER_CAUSE_DIMENSION = "cause";
+    private final        Counter tokenInvalidSignatureCounter;
+    private final        Counter tokenExpiredCounter;
+    private final        Counter tokenUnsupportedCounter;
+    private final        Counter tokenMalformedCounter;
 
     public SecurityErrorMeterServiceImpl(MeterRegistry registry) {
         this.tokenInvalidSignatureCounter = invalidTokensCounterForCauseBuilder("invalid-signature").register(registry);
