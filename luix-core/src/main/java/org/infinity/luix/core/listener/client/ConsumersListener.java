d66fb1d20d083f6205a5b053d10ea89a2df81f2f
刘尧 <liuyao@xforceplus.com>
2022-02-04 13:20:04 +0800

    refactor registry

--- luix-core/src/main/java/org/infinity/luix/core/listener/client/ConsumersListener.java
+++ luix-core/src/main/java/org/infinity/luix/core/listener/client/ConsumersListener.java
@@ -22,9 +22,9 @@ import java.util.List;
 
 
 /**
- * Listener used to handle the subscribed event to one consumer.
+ * Listener used to handle the subscribed event to all consumers.
  */
-public interface ConsumerListener {
+public interface ConsumersListener {
 
     /**
      * Called by the event which is subscribed.
