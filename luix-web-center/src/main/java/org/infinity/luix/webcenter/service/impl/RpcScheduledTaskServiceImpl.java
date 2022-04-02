ef2ed5b2c80096a296ba8ebd7d67c034c426848d
刘尧 <liuyao@xforceplus.com>
2022-04-02 18:46:04 +0800

    refactor scheduled task

--- luix-web-center/src/main/java/org/infinity/luix/webcenter/service/impl/RpcScheduledTaskServiceImpl.java
+++ luix-web-center/src/main/java/org/infinity/luix/webcenter/service/impl/RpcScheduledTaskServiceImpl.java
@@ -87,8 +87,8 @@ public class RpcScheduledTaskServiceImpl implements RpcScheduledTaskService, App
         saveUpdateStatusTask(registryUrl, RpcServerService.class.getName(), 5L);
         saveUpdateStatusTask(registryUrl, RpcServiceService.class.getName(), 2L);
 
-        saveLoadAllTask(registryUrl, RpcApplicationService.class.getName(), 10L, 30L);
-        saveLoadAllTask(registryUrl, RpcServerService.class.getName(), 10L, 30L);
+        saveLoadAllTask(registryUrl, RpcApplicationService.class.getName(), 10L, 10L);
+        saveLoadAllTask(registryUrl, RpcServerService.class.getName(), 10L, 10L);
     }
 
     private void saveUpdateStatusTask(String registryUrl, String interfaceName, Long interval) {
