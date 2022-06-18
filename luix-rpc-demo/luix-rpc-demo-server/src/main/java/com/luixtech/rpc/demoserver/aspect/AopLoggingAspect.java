4310d7b5603c097c5ef432dacf923798002d54bd
刘尧 <liuyao@xforceplus.com>
2022-06-18 14:57:09 +0800

    refactor

--- luix-rpc-demo/luix-rpc-demo-server/src/main/java/com/luixtech/rpc/demoserver/aspect/AopLoggingAspect.java
+++ luix-rpc-demo/luix-rpc-demo-server/src/main/java/com/luixtech/rpc/demoserver/aspect/AopLoggingAspect.java
@@ -1,6 +1,6 @@
 package com.luixtech.rpc.demoserver.aspect;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 import com.luixtech.rpc.demoserver.config.ApplicationProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
