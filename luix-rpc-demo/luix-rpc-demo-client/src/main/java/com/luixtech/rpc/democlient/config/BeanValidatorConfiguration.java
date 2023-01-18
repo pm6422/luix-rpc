179e026f1544151dfb71ab5d7b8b74a9e8e0dc8c
Louis Lau <pm6422@126.com>
2023-01-18 10:40:09 +0800

    Update BeanValidatorConfiguration.java

--- luix-rpc-demo/luix-rpc-demo-client/src/main/java/com/luixtech/rpc/democlient/config/BeanValidatorConfiguration.java
+++ luix-rpc-demo/luix-rpc-demo-client/src/main/java/com/luixtech/rpc/democlient/config/BeanValidatorConfiguration.java
@@ -1,10 +1,10 @@
 package com.luixtech.rpc.democlient.config;
 
 import org.springframework.context.MessageSource;
-import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import lombok.AllArgsConstructor;
 
 @Configuration
 public class BeanValidatorConfiguration {
