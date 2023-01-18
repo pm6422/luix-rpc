1a5b9c2197c17f63b60124055b6bbcf229aed72a
Louis Lau <pm6422@126.com>
2023-01-18 10:40:33 +0800

    Update HttpHeaderCreator.java

--- luix-rpc-demo/luix-rpc-demo-server/src/main/java/com/luixtech/rpc/demoserver/component/HttpHeaderCreator.java
+++ luix-rpc-demo/luix-rpc-demo-server/src/main/java/com/luixtech/rpc/demoserver/component/HttpHeaderCreator.java
@@ -1,6 +1,7 @@
 package com.luixtech.rpc.demoserver.component;
 
 import com.luixtech.rpc.demoserver.config.ApplicationConstants;
+import lombok.AllArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
