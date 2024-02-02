bba3cd6f7b98f875a3633e5e236b22bb7ee2323b
pm6422 <pm6422@126.com>
2024-02-02 21:35:48 +0800

    refactor

--- luix-rpc-web-center/src/main/java/com/luixtech/rpc/webcenter/controller/AccountController.java
+++ luix-rpc-web-center/src/main/java/com/luixtech/rpc/webcenter/controller/AccountController.java
@@ -19,7 +19,6 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.security.Principal;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
