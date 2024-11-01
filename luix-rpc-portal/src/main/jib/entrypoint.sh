08995bbc0fa425f0cd67ab348fcc071a1fc73ba5
pm6422 <pm6422@126.com>
2024-11-01 21:38:45 +0100

    refactor

--- luix-rpc-portal/src/main/jib/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "The application will start in ${SLEEP_TIME}s..." && sleep ${SLEEP_TIME}
-# JAVA_OPTS e.g. -Dspring.profiles.active=demo
-exec java -jar quarkus-app/quarkus-run.jar "$@"
