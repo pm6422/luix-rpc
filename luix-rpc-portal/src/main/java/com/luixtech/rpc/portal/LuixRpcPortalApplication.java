c1b548c5f24b4d43e2167728260b88ad989bfb82
pm6422 <pm6422@126.com>
2024-11-01 20:41:58 +0100

    introduce quarkus

--- luix-rpc-portal/src/main/java/com/luixtech/rpc/portal/LuixRpcPortalApplication.java
+++ luix-rpc-portal/src/main/java/com/luixtech/rpc/portal/LuixRpcPortalApplication.java
@@ -2,14 +2,11 @@ package com.luixtech.rpc.portal;
 
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.annotations.QuarkusMain;
-import lombok.extern.slf4j.Slf4j;
 
 @QuarkusMain
-@Slf4j
 public class LuixRpcPortalApplication {
 
     public static void main(String... args) {
-        log.info("Running main method");
         Quarkus.run(args);
     }
 }
